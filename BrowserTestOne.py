a7f1ce564417c980c0295b368d0f1b7c25185a7f
atul9404 <atul9404@gmail.com>
2019-11-22 19:00:48 +0530

    Second Commit on 22-11 for Chrome Execution

--- BrowserTestOne.py
+++ BrowserTestOne.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 import time
 
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+#   driver = webdriver.Firefox(GeckoDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 #   driver=webdriver.Firefox(executable_path='/home/atul/Downloads/geckodriver-v0.26.0-linux64/geckodriver.exe')
 
@@ -14,9 +15,10 @@ applicationTitle=driver.title
 
 print("\n"+applicationTitle)
 
-driver.find_element_by_name("email").send_keys("davidbird186@gmail.com")
-driver.find_element_by_name("pwd").send_keys("Fuck@123#")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div/div/form/div/div[3]/button').click()
+driver.find_element_by_xpath("//input[@name='email']").send_keys("davidbird186@gmail.com")
+driver.find_element_by_xpath("//input[@name='pwd']").send_keys("Fuck@123#")
+
+driver.find_element_by_xpath("//button[@type='submit']").click()
 
 time.sleep(5)
 
